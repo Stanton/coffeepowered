355c9df97b82eb77f6df518ae6ea1d68555a033f
Paul Stanton <paul.stanton@jadu.net>
2015-05-05 11:48:16 +0100

    Update root domain

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ config = {
     // When running Ghost in the wild, use the production environment
     // Configure your URL and mail settings here
     production: {
-        url: 'http://coffeepowered.co.uk',
+        url: 'http://www.coffeepowered.co.uk',
         mail: {},
         database: {
             client: 'sqlite3',
